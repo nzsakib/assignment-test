4e8c8595cab64f47b23f61fb1bbd7ccebdf751e0
Nazmus Sakib <sukku.mia@gmail.com>
2021-10-31 19:40:58 +0600

    Added readme instructions

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -15,11 +15,13 @@ class DashboardController extends Controller
             'month' => 'numeric|min:1|max:12|nullable',
         ]);
 
+        // Make the DTO
         $reqData = new UserFilterData([
             'month' => $request->get('month'),
             'year'  => $request->get('year'),
         ]);
 
+        // Get result by applying the filters
         $data = (new GetUserByBirthdayAction())->execute($reqData, $request->get('page', 1));
 
         return view('dashboard', [
